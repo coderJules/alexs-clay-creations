4498015211aa0680c912c352ab613b926df4e2b1
Julie VanderLaan julieannevl@gmail.com <julieannevl@gmail.com>
2019-01-25 15:41:04 +0100

    added landscape versions for mobile and ipad

--- components.js
+++ components.js
@@ -24,3 +24,70 @@ window.onclick = function(event) {
   }
 }
 
+
+$(function(){  // $(document).ready shorthand
+
+});
+
+$(document).ready(function() {
+
+    /* Every time the window is scrolled ... */
+    $(window).scroll( function(){
+
+        /* Check the location of each desired element */
+        $('.fade-in').each( function(i){
+
+            var bottom_of_object = $(this).position().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+
+            /* If the object is completely visible in the window, fade it it */
+            if( bottom_of_window > bottom_of_object ){
+
+                $(this).fadeTo(800, 1);
+
+            }
+
+        });
+
+    });
+
+});
+
+
+$(document).ready(function(){
+   var scroll_start = 0;
+   var startchange = $('.page-text');
+   var offset = startchange.offset();
+   $(document).scroll(function() {
+      scroll_start = $(this).scrollTop();
+      if(scroll_start < offset.top) {
+          $('.mobile-dropdown-nav').addClass('transparent-color');
+          $('.mobile-dropdown-nav').removeClass('background-color');
+       } else {
+           $('.mobile-dropdown-nav').removeClass('transparent-color');
+            $('.mobile-dropdown-nav').addClass('background-color');
+
+       }
+   });
+});
+
+// $(document).ready(function(){
+//    var scroll_start = 0;
+//    var startchange = $('.page-text');
+//    var offset = startchange.offset();
+//    $(document).scroll(function() {
+//       scroll_start = $(this).scrollTop();
+//       if(scroll_start < offset.top) {
+//           $('.background-color').css('background-color', 'rgba(70, 80, 90, .9');
+//        } else {
+//           $('.background-color').css('background-color', 'rgba(70, 80, 90, .9)');
+//        }
+//    });
+// });
+
+$(document).ready(function(){
+ if($(window).width()<500){
+  $('.mobile-dropdown-nav').addClass('background-color');
+  $('.mobile-dropdown-nav').addClass('transparent-color');
+ }
+});
